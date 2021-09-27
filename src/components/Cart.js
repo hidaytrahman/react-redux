d6be30059c77a317ca17dff4cda8f529aaa9d9ff
Hidayt Rahman <hidaytrahman@gmail.com>
2021-09-27 19:02:43 +0530

    saving cart data on the firebase database

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,51 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "store/cart-slice";
 
 const Cart = () => {
-    const cartItems = useSelector(state => state.cart.items);
+    const cart = useSelector(state => state.cart);
+    const cartItems = cart.items;
     const totalAmount = useSelector(state => state.cart.totalAmount);
 
     const dispatch = useDispatch();
 
     const removeFromCartHandler = (id) => {
+
         dispatch(cartActions.removeFromCart(id))
     }
 
     const addToCartHandler = (item) => {
-        dispatch(cartActions.addToCart(item))
+        dispatch(cartActions.addToCart(item));
+
+
+
     }
+
+    useEffect(() => {
+
+        // save item to cart
+        const saveCartItem = async () => {
+            const response = await fetch('https://react-app-61002-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart)
+            });
+
+            console.log('response ', response)
+
+            return response.json();
+
+        }
+
+        try {
+            const data = saveCartItem();
+            console.log(' data ', data);
+        } catch (err) {
+            throw new Error('Coundnt store the cart data')
+        }
+
+
+    }, [cart]);
+
 
     return (
         <div className="container">
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -54,6 +54,7 @@ const cartSlice = createSlice({
                 existingItem.quantity--;
                 existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
             }
+
         }
     }
 });
